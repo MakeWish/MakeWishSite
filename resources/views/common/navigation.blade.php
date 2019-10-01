86bd509fcf6bfd5f70a29573597cffcb4905a161
reifred <reifred33@gmail.com>
2019-10-01 13:38:43 +0300

    chore(setup-project): Create boiler plate for the project - Setup boiler plate for the project - Add navigation bar and footer
    [Finishes #168704451]

---
+++ resources/views/common/navigation.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+  <a class="navbar-brand " href="#">MakeWish</a>
+
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#services">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#projects">Projects</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#blog">Blog</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+
+</nav>
